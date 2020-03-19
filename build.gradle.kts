59bb839d4e4d6e319bcc00629682f64e8ab1412b
Konstantin Chukharev <lipen00@gmail.com>
2020-03-19 15:18:33 +0300

    Fix test configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -61,14 +62,13 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Test> {
-    @Suppress("UnstableApiUsage")
     useJUnitPlatform()
-    testLogging.events(
-        // TestLogEvent.PASSED,
-        TestLogEvent.FAILED,
-        TestLogEvent.SKIPPED,
-        TestLogEvent.STANDARD_ERROR
-    )
+    testLogging {
+        showExceptions = true
+        showStandardStreams = true
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED)
+        exceptionFormat = TestExceptionFormat.FULL
+    }
 }
 
 tasks.wrapper {
