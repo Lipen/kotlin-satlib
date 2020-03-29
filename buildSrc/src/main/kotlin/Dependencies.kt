bc59bf5c7893324c5144cd81131874de576c68f5
Konstantin Chukharev <lipen00@gmail.com>
2020-03-30 00:46:07 +0300

    Bump deps

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,14 @@
 @file:Suppress("PublicApiImplicitType")
 
 object Versions {
-    const val kotlin = "1.3.70"
+    const val kotlin = "1.3.71"
     const val kotlinter = "2.3.2"
     const val gradle_versions = "0.28.0"
     const val jgitver = "0.9.1"
     const val gradle_download = "4.0.4"
-    const val junit = "5.6.0"
+    const val junit = "5.6.1"
     const val kluent = "1.60"
-    const val okio = "2.4.3"
+    const val okio = "2.5.0"
     const val multiarray = "0.6.1"
     const val kotlin_jnisat = "0.8.3"
 }
