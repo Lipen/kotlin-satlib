bdbb9e4adac8c5ca3e44a929b623c9673a73b497
Konstantin Chukharev <lipen00@gmail.com>
2020-07-05 15:24:24 +0300

    Add logging to runWithTimeout

--- kotlin-satlib-core/src/main/kotlin/com/github/lipen/satlib/op/Timeout.kt
+++ kotlin-satlib-core/src/main/kotlin/com/github/lipen/satlib/op/Timeout.kt
@@ -1,5 +1,6 @@
 package com.github.lipen.satlib.op
 
+import com.github.lipen.satlib.solver.GlucoseSolver
 import com.github.lipen.satlib.solver.MiniSatSolver
 import com.github.lipen.satlib.solver.Solver
 import kotlinx.coroutines.Dispatchers
@@ -8,19 +9,32 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 
+private val log = mu.KotlinLogging.logger {}
+
 fun <T> Solver.runWithTimeout(timeMillis: Long, block: Solver.() -> T): T =
     runBlocking {
+        log.debug { "runWithTimeout(timeMillis = $timeMillis)" }
         val job = async(Dispatchers.Default) { block() }
         try {
             withTimeout(timeMillis) {
-                job.await()
+                log.debug { "Awaiting with timeout..." }
+                job.await().also {
+                    log.debug { "Result after awaiting is '$it'" }
+                }
             }
         } catch (e: TimeoutCancellationException) {
+            log.debug { "Timeout! Interrupting..." }
             interrupt()
-            job.await()
+            log.debug { "Awaiting after interrupt..." }
+            job.await().also {
+                log.debug { "Result after awaiting is '$it'" }
+            }
         } finally {
-            if (this@runWithTimeout is MiniSatSolver) {
-                backend.clearInterrupt()
+            // Actually, this must be done outside, but we are trying to do it here just to be safe
+            // Note: add your solver here if it supports resetting the interrupt state.
+            when (this@runWithTimeout) {
+                is MiniSatSolver -> backend.clearInterrupt()
+                is GlucoseSolver -> backend.clearInterrupt()
             }
         }
     }
