d32c4643a1e8fee65b8637bea844517ff1551e53
Konstantin Chukharev <lipen00@gmail.com>
2022-02-03 03:11:51 +0300

    Remove unnecessary imports

--- nexus/src/main/kotlin/com/github/lipen/satlib/nexus/cli/EqCheckCommand.kt
+++ nexus/src/main/kotlin/com/github/lipen/satlib/nexus/cli/EqCheckCommand.kt
@@ -2,7 +2,6 @@ package com.github.lipen.satlib.nexus.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.choice
