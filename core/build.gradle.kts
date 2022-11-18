977bc6a9077cea2dc58bd639bff1150823540fa7
Gunkin Anton <ntngunkin@yandex-team.ru>
2022-11-18 17:55:52 +0300

    change implementation to api for MultiArray dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,9 +2,9 @@ dependencies {
     api(project(":jni"))
     implementation(project(":utils"))
 
+    api(Libs.MultiArray.multiarray)
     implementation(Libs.KotlinLogging.kotlin_logging)
     implementation(Libs.Okio.okio)
-    implementation(Libs.MultiArray.multiarray)
     implementation(Libs.KotlinxCoroutines.kotlinx_coroutines_core)
 
     testImplementation(Libs.Klock.klock_jvm)
