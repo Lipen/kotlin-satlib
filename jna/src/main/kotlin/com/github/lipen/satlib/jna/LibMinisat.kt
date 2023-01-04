183faad273fb5f8469c012e00904cc55b233cdb4
Konstantin Chukharev <lipen00@gmail.com>
2023-01-04 23:43:12 +0400

    Change typealias names, fix return types

--- jna/src/main/kotlin/com/github/lipen/satlib/jna/LibMinisat.kt
+++ jna/src/main/kotlin/com/github/lipen/satlib/jna/LibMinisat.kt
@@ -6,52 +6,52 @@ import com.sun.jna.Library
 import com.sun.jna.Pointer
 import com.sun.jna.PointerType
 
-typealias Var = Int
-typealias Lit = Int
-typealias lbool = Int
+private typealias minisat_Var = Int
+private typealias minisat_Lit = Int
+private typealias minisat_lbool = Int
 
 interface LibMinisat : Library {
     fun minisat_signature(): String
     fun minisat_init(): CMinisat
     fun minisat_release(ptr: CMinisat)
 
-    fun minisat_newVar(ptr: CMinisat): Var
-    fun minisat_newLit(ptr: CMinisat): Lit
+    fun minisat_newVar(ptr: CMinisat): minisat_Var
+    fun minisat_newLit(ptr: CMinisat): minisat_Lit
 
-    fun minisat_mkLit(x: Var): Lit
-    fun minisat_mkLit_args(x: Var, sign: Int): Lit
-    fun minisat_negate(p: Lit): Lit
+    fun minisat_mkLit(x: minisat_Var): minisat_Lit
+    fun minisat_mkLit_args(x: minisat_Var, sign: Int): minisat_Lit
+    fun minisat_negate(p: minisat_Lit): minisat_Lit
 
-    fun minisat_var(p: Lit): Var
-    fun minisat_sign(p: Lit): Boolean
+    fun minisat_var(p: minisat_Lit): minisat_Var
+    fun minisat_sign(p: minisat_Lit): Boolean
 
     fun minisat_addClause(ptr: CMinisat, len: Int, ps: Pointer /* Lit* */): Boolean
     fun minisat_addClause_begin(ptr: CMinisat)
-    fun minisat_addClause_addLit(ptr: CMinisat, p: Lit)
+    fun minisat_addClause_addLit(ptr: CMinisat, p: minisat_Lit)
     fun minisat_addClause_commit(ptr: CMinisat): Boolean
 
     fun minisat_simplify(ptr: CMinisat): Boolean
 
     fun minisat_solve(ptr: CMinisat, len: Int, ps: Pointer /* Lit* */): Boolean
-    fun minisat_limited_solve(ptr: CMinisat, len: Int, ps: Pointer /* Lit* */): lbool
+    fun minisat_limited_solve(ptr: CMinisat, len: Int, ps: Pointer /* Lit* */): minisat_lbool
     fun minisat_solve_begin(ptr: CMinisat)
-    fun minisat_solve_addLit(ptr: CMinisat, p: Lit)
+    fun minisat_solve_addLit(ptr: CMinisat, p: minisat_Lit)
     fun minisat_solve_commit(ptr: CMinisat): Boolean
-    fun minisat_limited_solve_commit(ptr: CMinisat): lbool
+    fun minisat_limited_solve_commit(ptr: CMinisat): minisat_lbool
 
     fun minisat_okay(ptr: CMinisat): Boolean
 
-    fun minisat_setPolarity(ptr: CMinisat, v: Var, b: Int)
-    fun minisat_setDecisionVar(ptr: CMinisat, v: Var, b: Int)
+    fun minisat_setPolarity(ptr: CMinisat, v: minisat_Var, b: Int)
+    fun minisat_setDecisionVar(ptr: CMinisat, v: minisat_Var, b: Int)
 
-    fun minisat_get_l_True(): lbool
-    fun minisat_get_l_False(): lbool
-    fun minisat_get_l_Undef(): lbool
+    fun minisat_get_l_True(): minisat_lbool
+    fun minisat_get_l_False(): minisat_lbool
+    fun minisat_get_l_Undef(): minisat_lbool
 
-    fun minisat_value_Var(ptr: CMinisat, x: Var): lbool
-    fun minisat_value_Lit(ptr: CMinisat, p: Lit): lbool
-    fun minisat_modelValue_Var(ptr: CMinisat, x: Var): lbool
-    fun minisat_modelValue_Lit(ptr: CMinisat, p: Lit): lbool
+    fun minisat_value_Var(ptr: CMinisat, x: minisat_Var): minisat_lbool
+    fun minisat_value_Lit(ptr: CMinisat, p: minisat_Lit): minisat_lbool
+    fun minisat_modelValue_Var(ptr: CMinisat, x: minisat_Var): minisat_lbool
+    fun minisat_modelValue_Lit(ptr: CMinisat, p: minisat_Lit): minisat_lbool
 
     fun minisat_num_assigns(ptr: CMinisat): Int
     fun minisat_num_clauses(ptr: CMinisat): Int
@@ -60,7 +60,7 @@ interface LibMinisat : Library {
     fun minisat_num_freeVars(ptr: CMinisat): Int
 
     fun minisat_conflict_len(ptr: CMinisat): Int
-    fun minisat_conflict_nthLit(ptr: CMinisat, i: Int): Lit
+    fun minisat_conflict_nthLit(ptr: CMinisat, i: Int): minisat_Lit
 
     fun minisat_set_conf_budget(ptr: CMinisat, x: Int)
     fun minisat_set_prop_budget(ptr: CMinisat, x: Int)
@@ -69,8 +69,8 @@ interface LibMinisat : Library {
     fun minisat_interrupt(ptr: CMinisat)
     fun minisat_clearInterrupt(ptr: CMinisat)
 
-    fun minisat_setFrozen(ptr: CMinisat, v: Var, b: Boolean)
-    fun minisat_isEliminated(ptr: CMinisat, v: Var): Boolean
+    fun minisat_setFrozen(ptr: CMinisat, v: minisat_Var, b: Boolean)
+    fun minisat_isEliminated(ptr: CMinisat, v: minisat_Var): Boolean
     fun minisat_eliminate(ptr: CMinisat, turn_off_elim: Boolean): Boolean
 
     fun minisat_set_verbosity(ptr: CMinisat, v: Int)
@@ -89,28 +89,37 @@ interface LibMinisat : Library {
     }
 }
 
-fun LibMinisat.minisat_addClause(ptr: LibMinisat.CMinisat, clause: Iterable<Lit>) {
+fun LibMinisat.minisat_addClause(
+    ptr: LibMinisat.CMinisat,
+    clause: Iterable<minisat_Lit>,
+): Boolean {
     minisat_addClause_begin(ptr)
     for (lit in clause) {
         minisat_addClause_addLit(ptr, lit)
     }
-    minisat_addClause_commit(ptr)
+    return minisat_addClause_commit(ptr)
 }
 
-fun LibMinisat.minisat_solve(ptr: LibMinisat.CMinisat, assumptions: Iterable<Lit> = emptyList()) {
+fun LibMinisat.minisat_solve(
+    ptr: LibMinisat.CMinisat,
+    assumptions: Iterable<minisat_Lit> = emptyList(),
+): Boolean {
     minisat_solve_begin(ptr)
     for (lit in assumptions) {
         minisat_solve_addLit(ptr, lit)
     }
-    minisat_solve_commit(ptr)
+    return minisat_solve_commit(ptr)
 }
 
-fun LibMinisat.minisat_limited_solve(ptr: LibMinisat.CMinisat, assumptions: Iterable<Lit> = emptyList()) {
+fun LibMinisat.minisat_limited_solve(
+    ptr: LibMinisat.CMinisat,
+    assumptions: Iterable<minisat_Lit> = emptyList(),
+): minisat_lbool {
     minisat_solve_begin(ptr)
     for (lit in assumptions) {
         minisat_solve_addLit(ptr, lit)
     }
-    minisat_limited_solve_commit(ptr)
+    return minisat_limited_solve_commit(ptr)
 }
 
 fun main() {
@@ -129,7 +138,7 @@ fun main() {
     println("y = $y")
     println("-y = ${lib.minisat_negate(y)}")
 
-    fun addClause(clause: Iterable<Lit>) {
+    fun addClause(clause: Iterable<minisat_Lit>) {
         val lits = clause.toList()
         println("Adding clause $lits")
         lib.minisat_addClause(ptr, lits)
