4085b69cf21b3bd05a6ac50df5bfba362e2effb2
Konstantin Chukharev <lipen00@gmail.com>
2023-02-18 23:59:41 +0400

    Replace BoolVarArray with typealias

--- kotlin-satlib-core/src/main/kotlin/com/github/lipen/satlib/core/VarArray.kt
+++ kotlin-satlib-core/src/main/kotlin/com/github/lipen/satlib/core/VarArray.kt
@@ -1,34 +1,13 @@
 package com.github.lipen.satlib.core
 
 import com.github.lipen.multiarray.Index
-import com.github.lipen.multiarray.IntMultiArray
 import com.github.lipen.multiarray.MultiArray
 import com.github.lipen.multiarray.Shape
 import com.github.lipen.satlib.solver.Solver
 
 typealias DomainVarArray<T> = MultiArray<DomainVar<T>>
 typealias IntVarArray = MultiArray<IntVar>
-
-class BoolVarArray @PublishedApi internal constructor(
-    private val backend: IntMultiArray,
-) : MultiArray<Lit> by backend {
-    companion object {
-        @JvmStatic
-        @JvmName("newVararg")
-        inline fun new(
-            vararg shape: Int,
-            zerobased: Boolean = false,
-            init: (Index) -> Lit,
-        ): BoolVarArray = new(shape, zerobased, init)
-
-        @JvmStatic
-        inline fun new(
-            shape: IntArray,
-            zerobased: Boolean = false,
-            init: (Index) -> Lit,
-        ): BoolVarArray = BoolVarArray(IntMultiArray.new(Shape(shape), zerobased, init))
-    }
-}
+typealias BoolVarArray = MultiArray<Lit>
 
 inline fun <T> Solver.newDomainVarArray(
     vararg shape: Int,
@@ -54,7 +33,7 @@ inline fun Solver.newBoolVarArray(
     vararg shape: Int,
     zerobased: Boolean = false,
     init: (Index) -> Lit = { newLiteral() },
-): BoolVarArray = BoolVarArray.new(shape, zerobased, init)
+): BoolVarArray = BoolVarArray.new(Shape(shape), zerobased, init)
 
 val <T> DomainVarArray<T>.literals: List<Lit>
     get() = values.flatMap { it.literals }
